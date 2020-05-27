cb06db4c800c6f410ed1c24c7e784d57229a31bf
maxlath <k@maxlath.eu>
2020-05-27 10:04:28 +0200

    Replace new Buffer by Buffer.from
    to remove deprecation warning in Node >= v6

--- ttf2woff.js
+++ ttf2woff.js
@@ -68,7 +68,7 @@ if (args.metadata) {
 
 var ttf = new Uint8Array(input);
 //var ttf = Array.prototype.slice.call(input, 0);
-var woff = new Buffer(ttf2woff(ttf, options).buffer);
+var woff = Buffer.from(ttf2woff(ttf, options).buffer);
 
 fs.writeFileSync(args.outfile[0], woff);
 
